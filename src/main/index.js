7b57dd60727c6d8a138720339753fc55786e6b10
rentalname <agenda21st@gmail.com>
2017-06-16 05:18:05 +0900

    キャプチャー範囲を指定する機能を実装

---
+++ src/main/index.js
@@ -0,0 +1,8 @@
+import { app } from 'electron'
+import trimDesktop from './trimDesktop'
+
+app.on('ready', ()=>{
+  trimDesktop().then(({sourceDisplay, trimmedBounds})=>{
+    console.log(sourceDisplay, trimmedBounds)
+  })
+})
